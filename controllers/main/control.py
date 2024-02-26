89552308f880a4844a5f8ea6703847ad7310e1ec
ben-jarvis <ben_jarvis@outlook.com>
2024-02-26 23:35:13 +0100

    Update documentation

--- controllers/main/control.py
+++ controllers/main/control.py
@@ -25,16 +25,6 @@ class quadrotor_controller():
 
         # Only for tuning
         self.tuning_level = "off" # Exercise 1: Choose what to tune ["vel_z", "pos_z", "rate_rp", ("rate_y"), "att_rp", ("att_y"), "vel_xy", "pos_xy"]
-
-        # good gains
-        # gains = {"P_vel_z": 6.0,     "I_vel_z": 1.0,     "D_vel_z": 0.8,
-        #             "P_pos_z": 2.5,     "I_pos_z": 0.0,     "D_pos_z": 1.0,
-        #             "P_rate_rp": 0.5,     "I_rate_rp":0.0,      "D_rate_rp": 0.03,
-        #             "P_rate_y": 0.01,      "I_rate_y": 0.0,      "D_rate_y": 0.001,
-        #             "P_att_rp": 18.0,     "I_att_rp":0.0,      "D_att_rp": 0.3,
-        #             "P_att_y": 5.0,      "I_att_y": 0.0,      "D_att_y": 0.1,
-        #             "P_vel_xy": 5.0,     "I_vel_xy": 0.0,     "D_vel_xy": 0.15,
-        #             "P_pos_xy": 2.2,     "I_pos_xy": 0.0,     "D_pos_xy": 0.03}
         
         # Exercise 1: Tune gains (we suggest: P < 25, I = 0 in most cases, D < 2)
         gains = {"P_vel_z": 4.0,     "I_vel_z": 1.0,     "D_vel_z": 0.1,
@@ -54,15 +44,6 @@ class quadrotor_controller():
                 "L_vel_z": 0.75,
                 "L_vel_xy": 2.0
         }
-
-        # crazy limits
-        # self.limits = {
-        #         "L_rate_rp": 2.0,
-        #         "L_rate_y": 3.0,
-        #         "L_acc_rp": 10.0,
-        #         "L_vel_z": 1.0,
-        #         "L_vel_xy": 3.0
-        # }
 
         self.tuning_on = False
         self.tuning_start = 7
