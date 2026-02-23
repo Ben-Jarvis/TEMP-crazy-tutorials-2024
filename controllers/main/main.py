1887fb36a79397087ed4e9195c79045d1f18727b
ben-jarvis <ben_jarvis@outlook.com>
2026-02-23 14:03:51 +0100

    Change rotation function in main.py

--- controllers/main/main.py
+++ controllers/main/main.py
@@ -14,7 +14,7 @@ import lib.mapping_and_planning_examples as mapping_and_planning_examples
 import time, random
 import threading
 
-exp_num = 1                    # 0: Coordinate Transformation, 1: PID Tuning, 2: Kalman Filter, 3: Motion Planning, 4: Project
+exp_num = 0                    # 0: Coordinate Transformation, 1: PID Tuning, 2: Kalman Filter, 3: Motion Planning, 4: Project
 control_style = 'keyboard'      # 'keyboard' or 'path_planner'
 rand_env = False                # Randomise the environment
 
@@ -699,7 +699,7 @@ if __name__ == '__main__':
                     # Rotate the control commands from the body reference frame to the inertial reference frame
                     euler_angles = [sensor_data['roll'], sensor_data['pitch'], sensor_data['yaw']]
                     quaternion = [sensor_data['q_x'], sensor_data['q_y'], sensor_data['q_z'], sensor_data['q_w']]
-                    control_commands = ex0_rotations.rot_body2inertial(control_commands, euler_angles, quaternion)
+                    control_commands = ex0_rotations.rot_inertial2body(control_commands, euler_angles, quaternion)
 
                     # Call the PID controller to get the motor commands
                     motorPower = drone.PID_CF.keys_to_pwm(drone.dt_ctrl, control_commands, sensor_data)    
